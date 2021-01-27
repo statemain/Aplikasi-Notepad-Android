bae501fa78b03b9e2a8ca4855a999926c5dff514
chainshelter <asepseptiadi.surel@gmail.com>
2021-01-27 20:23:56 +0700

    Update fiture list notes berdasarkan category notes To do dan Task #2

--- app/notesapp/app/src/main/java/ei/eseptiyadi/notesapp/views/DashboardActivity.java
+++ app/notesapp/app/src/main/java/ei/eseptiyadi/notesapp/views/DashboardActivity.java
@@ -23,7 +23,7 @@ import retrofit2.Response;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    RecyclerView rvListNotes, rvListTodo;
+    RecyclerView rvListTasks, rvListTodo, rvListNotes;
     SwipeRefreshLayout srvListNotes;
 
 
@@ -32,24 +32,39 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        rvListNotes = (RecyclerView)findViewById(R.id.rv_listtask);
+        rvListTasks = (RecyclerView)findViewById(R.id.rv_listtask);
         rvListTodo = (RecyclerView)findViewById(R.id.rv_listtodo);
+        rvListNotes = (RecyclerView)findViewById(R.id.rv_listnotes);
         srvListNotes = (SwipeRefreshLayout)findViewById(R.id.srlLoadList);
 
-        rvListNotes.setHasFixedSize(true);
+        rvListTasks.setHasFixedSize(true);
         rvListTodo.setHasFixedSize(true);
+        rvListNotes.setHasFixedSize(true);
 
-        rvListNotes.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        rvListTasks.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         rvListTodo.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        rvListNotes.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
         String userName = "septiadi";
         String userKey = "!!&21adi";
         String hashUser = "adminkantor";
         String levelUser = "Administrator";
 
-        loadDataTask(userName, userKey, hashUser, levelUser, "Task");
-        loadDataNotes(userName, userKey, hashUser, levelUser, "Notes");
-        loadDataTodos(userName, userKey, hashUser, levelUser, "To do");
+        updateList(userName, userKey, hashUser, levelUser);
+
+        srvListNotes.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                updateList(userName, userKey, hashUser, levelUser);
+            }
+        });
+
+    }
+
+    public void updateList(String username, String password, String hash, String level) {
+        loadDataTask(username, password, hash, level, "Task");
+        loadDataNotes(username, password, hash, level, "Notes");
+        loadDataTodos(username, password, hash, level, "To do");
     }
 
     private void loadDataTodos(String userName, String userKey, String hashUser, String levelUser, String to_do) {
@@ -74,6 +89,7 @@ public class DashboardActivity extends AppCompatActivity {
                     if (status == true) {
                         AdapterDashboard adapterDashboard = new AdapterDashboard(DashboardActivity.this, notesItemList);
                         rvListTodo.setAdapter(adapterDashboard);
+                        srvListNotes.setRefreshing(false);
                     } else {
                         Toast.makeText(DashboardActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
                     }
@@ -90,13 +106,48 @@ public class DashboardActivity extends AppCompatActivity {
     }
 
     private void loadDataNotes(String userName, String userKey, String hashUser, String levelUser, String notes) {
+        TextView totalDataNotes, titleNotes;
+        totalDataNotes = (TextView)findViewById(R.id.txTotalDataNotes);
+        titleNotes = (TextView)findViewById(R.id.txCategoryNotes);
+
+
+        ApiServices apiServices = RetrofitClient.getInstance();
+        Call<ResponseListNotes> responseListNotesCall = apiServices.reqListNotes(userName, userKey, hashUser,levelUser, notes);
+
+        responseListNotesCall.enqueue(new Callback<ResponseListNotes>() {
+            @Override
+            public void onResponse(Call<ResponseListNotes> call, Response<ResponseListNotes> response) {
+                if (response.isSuccessful()) {
+
+                    totalDataNotes.setText(response.body().getTotalnotes() + " Total Item");
+                    titleNotes.setText("Kategori " + response.body().getCategory());
+
+                    boolean status = response.body().isStatus();
+                    List<ListnotesItem> notesItemList = response.body().getListnotes();
+
+                    if (status == true) {
+                        AdapterDashboard adapterDashboard = new AdapterDashboard(DashboardActivity.this, notesItemList);
+                        rvListNotes.setAdapter(adapterDashboard);
+                        srvListNotes.setRefreshing(false);
+                    } else {
+                        Toast.makeText(DashboardActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(DashboardActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseListNotes> call, Throwable t) {
+
+            }
+        });
     }
 
     private void loadDataTask(String userName, String userKey, String hashUser, String levelUser, String task) {
         TextView totalDataTask, titleTask;
         totalDataTask = (TextView)findViewById(R.id.txTotalDataTask);
         titleTask = (TextView)findViewById(R.id.txCategoryTask);
-
 
         ApiServices apiServices = RetrofitClient.getInstance();
         Call<ResponseListNotes> responseListNotesCall = apiServices.reqListNotes(userName, userKey, hashUser,levelUser, task);
@@ -114,7 +165,8 @@ public class DashboardActivity extends AppCompatActivity {
 
                     if (status == true) {
                         AdapterDashboard adapterDashboard = new AdapterDashboard(DashboardActivity.this, notesItemList);
-                        rvListNotes.setAdapter(adapterDashboard);
+                        rvListTasks.setAdapter(adapterDashboard);
+                        srvListNotes.setRefreshing(false);
                     } else {
                         Toast.makeText(DashboardActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
                     }
