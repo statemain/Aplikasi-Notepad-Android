6174ead9d22711b12fd19417753d981404c7c79b
chainshelter <asepseptiadi.surel@gmail.com>
2021-02-25 17:21:45 +0700

    Update activity welcome

--- app/notesapp/app/src/main/java/ei/eseptiyadi/notesapp/views/WelcomeActivity.java
+++ app/notesapp/app/src/main/java/ei/eseptiyadi/notesapp/views/WelcomeActivity.java
@@ -1,4 +1,4 @@
-package ei.eseptiyadi.notesapp.auth;
+package ei.eseptiyadi.notesapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import ei.eseptiyadi.notesapp.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_welcome);
     }
 }
