68d78187843a20a23bd3793c0f100416d3691a64
chainshelter <asepseptiadi.surel@gmail.com>
2021-02-26 08:57:23 +0700

    Integrasi Activity Login & Memberikan Action Intent pada Button Login di Activity Welcome

--- app/notesapp/app/src/main/java/ei/eseptiyadi/notesapp/views/WelcomeActivity.java
+++ app/notesapp/app/src/main/java/ei/eseptiyadi/notesapp/views/WelcomeActivity.java
@@ -2,7 +2,11 @@ package ei.eseptiyadi.notesapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import ei.eseptiyadi.notesapp.auth.LoginActivity;
 
 import ei.eseptiyadi.notesapp.R;
 
@@ -12,5 +16,9 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void gotoSignin(View view) {
+        startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
     }
 }
